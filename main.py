a684688bc9a2bc8427798869a7309580645c2b72
Sportlich <remigijus.sportlich@gmail.com>
2019-09-13 09:18:09 +0100

    Added examples to help command, updated README

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 __author__ = 'gulliver - Radek Simkanic'
 
 import argparse
+from argparse import RawDescriptionHelpFormatter
 from core.renamer import Renamer
 
 
 def parseArguments():
     parser = argparse.ArgumentParser(
-        description="Rename multiple subtitles automatically")
+        description="Rename multiple subtitles automatically",
+        epilog=(
+            'Examples:\n'
+            '\tpython main.py -s txt -m avi .\n'
+            '\tpython main.py -s srt -m mp4 ../../Video/\n'
+            '\tpython main.py -s my_serial_series_01_episode_01.txt -m my_serial_SE01E01.avi ../../Video/\n'
+            '\tpython main.py -s /home/user/Video/my_serial_series_01_episode_01.txt -m /home/user/Video/my_serial_SE01E01.avi /home/user/Video/\n'
+            '\tpython main.py -s srt -m mp4 ../../Video/ -t'
+        ),
+        formatter_class=RawDescriptionHelpFormatter
+    )
 
     parser.add_argument('-s', '--subtitle', required=True,
-                        help="Subtitle file extension")
+                        help="Subtitle file extension, i.e. srt, txt, ass, etc.")
     parser.add_argument('-m', '--movie', required=True,
-                        help="Video file extension")
+                        help="Video file extension, i.e. avi, mkv, mp4, etc.")
     parser.add_argument('-d', '--directory', help="Directory containing files",
                         default=".")
     parser.add_argument('-t', '--test', action='store_true',
